d15d5e030eead5504aaba5f3c8c01dd68fa36c5b
qadrosIngenieros <68667847+qadrosIngenieros@users.noreply.github.com>
2020-07-22 17:21:39 -0500

    Add files via upload

---
+++ archivo.php
@@ -0,0 +1,54 @@
+<!doctype html>
+
+<html>
+
+
+        <head>
+
+    <meta charset="utf-8">
+
+           <title>Correo_QADROS-INGENIEROS</title>
+
+       </head>
+
+     <body>
+             <?php
+
+                
+                $Nombre= $_POST["Nombre"];
+                $Correo= $_POST["Correo"];
+                $Teléfono= $_POST["Teléfono"];
+                                
+                $mensaje= $_POST["mensaje"];
+                
+
+                $para= "qadros.ingenieros@gmail.com";
+
+
+                $asunto= "Mensaje Nuevo";
+                
+            $Mensaje = "   
+
+
+              Nombre del cliente: ".$Nombre."
+              email:              ".$Correo."
+              tel:               ".$Teléfono."
+              mensaje:            ".$mensaje."
+              
+
+              ";   
+
+                
+                
+               mail($para,$asunto,utf8_decode($Mensaje)); 
+
+
+	      echo "<p>Hemos recibido su mensaje correctamente, pronto le contestaremos, gracias<-p>";
+             ?>
+      
+
+     </body>
+
+<a href="../index.html"></a>
+
+</html>
